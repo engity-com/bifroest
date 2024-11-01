4642140c9e4867ea39c051bbacbf612dc64bc1fd
Gregor Noczinski <gregor@noczinski.eu>
2024-11-02 00:10:52 +0100

    Also implement cleanup rule.

--- cmd/build/repo-prs.go
+++ cmd/build/repo-prs.go
@@ -185,10 +185,10 @@ func (this *repoPr) deleteRelatedArtifacts(ctx context.Context) error {
 		return fmt.Errorf("cannot delete artifacts for %v: %w", this, err)
 	}
 
-	docsTag := fmt.Sprintf("docs/pr-%d", this.GetID())
+	docsTag := fmt.Sprintf("docs/pr-%d", this.GetNumber())
 	if err := this.parent.execute(ctx, "git", "push", "--delete", "origin", docsTag).
 		do(); err != nil {
-		log.With("pr", this.GetID()).
+		log.With("pr", this.GetNumber()).
 			With("tag", docsTag).
 			WithError(err).
 			Info("cannot delete tag in Git; this can be a problem or simply mean that the tag does not exist; ignoring...")
@@ -198,7 +198,7 @@ func (this *repoPr) deleteRelatedArtifacts(ctx context.Context) error {
 		return this.parent.actions.packages.deleteVersionsWithTags(ctx, tag)
 	}
 
-	mainTag := fmt.Sprintf("pr-%d", this.GetID())
+	mainTag := fmt.Sprintf("pr-%d", this.GetNumber())
 
 	if err := do(mainTag); err != nil {
 		return fail(err)
